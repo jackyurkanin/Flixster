97a0e1d761b57de9e0b7270a45b6acc8d6c969c5
jackyurkanin <62447001+jackyurkanin@users.noreply.github.com>
2021-07-22 12:09:27 -0700

    Update MainActivity.java

--- app/src/main/java/com/codepath/jackyurkanin/flixster/MainActivity.java
+++ app/src/main/java/com/codepath/jackyurkanin/flixster/MainActivity.java
@@ -24,7 +24,7 @@ import okhttp3.Headers;
 public class MainActivity extends AppCompatActivity {
 
     public static final String BASE_URL = "https://api.themoviedb.org/3";
-    public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+    public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=YOUR_API_KEY";
     public static final String TAG = "MainActivity";
 
     List<Movie> movies;
